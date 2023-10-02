e0a0a485e6fd001f29f3777b8e054ce6bdf4c8c1
Poonyawat Thareepatheep <poonyawatla@gmail.com>
2023-10-02 14:36:29 +0700

    commentzone
    แสดงคอมเม้นต์

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,20 @@ const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
 function App() {
   return (
       <Layout>
-        <Content style={{backgroundColor:"#ffedc2",height:"100vh"}}>
+        <Content style={{backgroundColor:"#F4F4F4",height:"100vh"}}>
           <div>
-              <div style={{justifyContent:"center",padding:"100px 150px"}}> <TextArea showCount maxLength={500} style={{ height: 120,width:600, resize: 'none' }} onChange={onChange}placeholder="Input allowed up to 500 characters"/>
+              <div style={{justifyContent:"center",padding:"100px 300px"}}> <TextArea showCount maxLength={500} style={{ height: 120,width:600, resize: 'none' }} onChange={onChange}placeholder="Input allowed up to 500 characters"/>
                   <div style ={{ height: 120,width:600, resize: 'none',marginTop:"10px" }}>
                     <Button type="primary">Post</Button>
                   </div>
                   <div>
                   <Divider>Comment</Divider>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci possimus dolores dolorum cumque corporis eum repellendus culpa rerum necessitatibus corrupti sint totam eaque omnis dolor odio velit itaque nemo voluptate, beatae quibusdam iste ducimus? Expedita illum modi quibusdam, a qui ducimus, repudiandae dignissimos eveniet dicta provident adipisci debitis reiciendis molestias ex maiores ratione enim culpa veniam? Numquam accusamus ea sequi sapiente quod voluptatem incidunt, commodi minima nisi pariatur, obcaecati dolor nesciunt vel reiciendis! Laborum eos, perspiciatis totam saepe dolor, odit iure quae ipsa, beatae velit cupiditate asperiores alias pariatur quibusdam non hic accusantium maxime ea officia! Beatae praesentium iste necessitatibus.</p>
+                  <Divider></Divider>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos, nihil, nostrum eum repellat eaque blanditiis id pariatur ullam debitis fugiat inventore repudiandae! Mollitia, ab vel necessitatibus alias sed quo quaerat.</p>
+                  <Divider></Divider>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eum vitae hic inventore perspiciatis. Accusantium eveniet qui asperiores? Quae culpa voluptatibus voluptas provident accusantium quo, et tenetur eum dolorem at!</p>
+                  
                   </div>
               </div>
           </div>
